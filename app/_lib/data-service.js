25d86e04c4f3aafcfd4b9fa48972fd7466b9fb03
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-18 08:31:10 +0530

    not found pages

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,4 +1,5 @@
 import { eachDayOfInterval } from "date-fns";
+import { notFound } from "next/navigation";
 import { supabase } from "./supabase";
 
 export async function getCabin(id) {
@@ -10,6 +11,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
